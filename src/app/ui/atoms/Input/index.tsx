f315a99d3983a885670f8bdd10960f68c37c54b9
phoby20 <54525527+phoby20@users.noreply.github.com>
2024-08-21 18:45:03 +0900

    add: 残りの数表示および生成ボタンの制御

--- src/app/ui/atoms/Input/index.tsx
+++ src/app/ui/atoms/Input/index.tsx
@@ -4,15 +4,22 @@ import styles from "./page.module.scss";
 
 type InputType = {
   type: string;
+  value: number | string;
   onChange?: (e: string) => void;
   placeholder: string;
 };
 
-export default function Input({ type, onChange, placeholder }: InputType) {
+export default function Input({
+  type,
+  value,
+  onChange,
+  placeholder,
+}: InputType) {
   return (
     <input
       className={styles.input}
       type={type}
+      value={String(value)}
       placeholder={placeholder}
       onChange={(e) => {
         if (onChange) {
--- src/app/ui/organisms/Header/index.tsx
+++ src/app/ui/organisms/Header/index.tsx
@@ -2,11 +2,21 @@
 
 import styles from "./page.module.scss";
 
-export default function Header() {
+type HeaderType = {
+  count: number;
+};
+
+export default function Header({ count }: HeaderType) {
   const logoText = "full count".toUpperCase();
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>{logoText}</div>
+      <div className={styles.right_box}>
+        <div className={styles.count}>
+          残り：<strong>{count}</strong>
+        </div>
+      </div>
     </div>
   );
 }
