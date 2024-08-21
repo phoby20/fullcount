b09ef764f7a742c7fcf7150e04516a3a66fee291
phoby20 <54525527+phoby20@users.noreply.github.com>
2024-08-21 19:54:40 +0900

    design: 스타일 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
 import Main from "./Main/page";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Head>
       <Main />
     </main>
   );
